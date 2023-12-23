2ce8342f9fdb7dd1245600148e3fae202bb35277
BilalMO <azbilal.motassim@gmail.com>
2023-12-23 18:20:38 +0100

    Add project files.

---
+++ LibraryWPF/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace LibraryWPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
